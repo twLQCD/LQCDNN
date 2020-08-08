f7c72c53da99853bef7ff5a0f7e41debece9bda9
isospinLQCD <Travis_Whyte@baylor.edu>
2020-08-07 21:18:28 -0500

    made things nicer

--- new files/math_helpers.py
+++ new files/math_helpers.py
@@ -8,19 +8,16 @@ import numpy as np
 import math
 
 def norm(psi, chi, p):
-    tmp = 0.0
+    scalar = 0.0
     for i in range(p.nt):
         for j in range(p.nx):
             for k in range(2):
-                tmp = tmp + np.real(psi[j][i].site[k])*np.real(chi[j][i].site[k])
-                tmp = tmp - np.imag(psi[j][i].site[k])*np.imag(chi[j][i].site[k])
+                scalar += np.conj(psi[j][i].site[k]) * chi[j][i].site[k]
     
-    return tmp
+    return scalar
 
-def normalize(a, psi, p):
+def normalize(scalar, psi, p):
     for i in range(p.nt):
         for j in range(p.nx):
             for k in range(2):
-                psi[j][i].site[k] = (1.0/ math.sqrt(a))*psi[j][i].site[k]
-    
-    return psi
+                psi[j][i].site[k] /= math.sqrt(scalar)
